d7d05ba819d4cb13a57d7aa6bea1987ca2c51a9c
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-01 07:04:02 +0700

    Refactored all codes into single quote

--- src/utils/useWindowSize.js
+++ src/utils/useWindowSize.js
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 export const useWindowSize = () => {
-  const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
+  const [size, setSize] = useState([window.innerWidth, window.innerHeight])
 
   useEffect(() => {
     const updateSize = () => {
-      setSize([window.innerWidth, window.innerHeight]);
-    };
+      setSize([window.innerWidth, window.innerHeight])
+    }
 
-    window.addEventListener("resize", updateSize);
+    window.addEventListener('resize', updateSize)
 
-    return () => window.removeEventListener("resize", updateSize);
-  }, []);
+    return () => window.removeEventListener('resize', updateSize)
+  }, [])
 
   return {
     width: size[0],
     height: size[1],
-  };
-};
+  }
+}
