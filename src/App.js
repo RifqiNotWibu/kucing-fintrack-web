ea580c6f86a942ae67208d1b46b28ffadccc9496
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-01 17:39:23 +0700

    Added useState and orbMemo stareMemo hook

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import styled from 'styled-components'
 import bg from './img/bg.png'
 import { MainLayout } from './styles/Layouts'
 import Orb from './Components/Orb/Orb.js'
-import Navigation from './Components/Navigation/navigation.js'
+import Navigation from './Components/Navigation/Navigation.js'
+import React, { useState, useMemo } from 'react'
 
 function App() {
+  const [active, setActive] = useState(1)
+
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  }, [])
+
   return (
     <AppStyled bg={bg} className='App'>
+      {orbMemo}
       <Orb />
       <MainLayout>
-        <Navigation />
+        <Navigation active={active} setActive={setActive} />
+        <main>
+          <h1>Hello</h1>
+        </main>
       </MainLayout>
     </AppStyled>
   )
@@ -19,7 +30,16 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${(props) => props.bg});
   position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
 `
 export default App
-
-//1:09:21
