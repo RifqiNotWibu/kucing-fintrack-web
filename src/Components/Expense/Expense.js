67b46cad97b42ff843db286e8e0fb859cfc91bc9
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-01 22:57:53 +0700

    Expense component init

--- src/Components/Expense/Expense.js
+++ src/Components/Expense/Expense.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
+import { InnerLayout } from '../../styles/Layouts'
 
 function Expense() {
-  return <ExpenseStyled>Expense</ExpenseStyled>
+  return (
+    <ExpenseStyled>
+      <InnerLayout>
+        <h1>Expense</h1>
+      </InnerLayout>
+    </ExpenseStyled>
+  )
 }
 
 const ExpenseStyled = styled.div``
