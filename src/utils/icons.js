b80629ba1ba744ef64033595453efc24e4412c64
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-01 07:49:12 +0700

    Added additional icons

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -2,3 +2,8 @@ export const dashboard = <i className='fa-solid fa-chart-line'></i>
 export const transactions = <i className='fa-solid fa-credit-card'></i>
 export const categories = <i className='fa-solid fa-tags'></i>
 export const accounts = <i className='fa-solid fa-wallet'></i>
+export const settings = <i className='fa-solid fa-cog'></i>
+export const logout = <i className='fa-solid fa-sign-out'></i>
+export const trend = <i className='fa-solid fa-money-bill-trend-up'></i>
+export const expenses = <i className='fa-solid fa-money-bill-transfer'></i>
+export const signout = <i className='fa-solid fa-right-from-bracket'></i>
