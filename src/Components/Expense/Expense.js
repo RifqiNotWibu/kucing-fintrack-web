3790bec259deec45e6437b74ce029bb23b330ba8
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-01 17:44:04 +0700

    Added Expense component

---
+++ src/Components/Expense/Expense.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Expense() {
+  return <ExpenseStyled>Expense</ExpenseStyled>
+}
+
+const ExpenseStyled = styled.div``
+
+export default Expense
