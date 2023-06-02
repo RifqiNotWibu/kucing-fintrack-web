07f1d1d43666e6ffa813c3e86da2440de8773dba
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-02 10:02:54 +0700

    Added title form

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+function Form() {
+  //   const { addIncome, getIncomes, error, setError } = useGlobalContext()
+  const [inputState, setInputState] = useState({
+    title: '',
+    amount: '',
+    date: '',
+    category: '',
+    description: '',
+  })
+
+  const { title, amount, date, category, description } = inputState
+
+  const handleInput = (name) => (e) => {
+    setInputState({ ...inputState, [name]: e.target.value })
+  }
+  return (
+    <FormStyled>
+      <div className='input-control'>
+        <input
+          type='text'
+          value={title}
+          name={'title'}
+          placeholder='Salary Title'
+          onChange={handleInput('title')}
+        />
+      </div>
+    </FormStyled>
+  )
+}
+
+const FormStyled = styled.form``
+
+export default Form
