5179ce1e0700e7e9e556dfc8862fa6ccac5f7cfa
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-03 23:40:36 +0700

    Changed orb size

--- src/Components/Orb/orb.js
+++ src/Components/Orb/orb.js
@@ -10,7 +10,7 @@ function Orb() {
             transform: translate(0, 0);
         }
         50%{
-            transform: translate(${width}px, ${height / 2}px);
+            transform: translate(${width / 1.5}px, ${height / 2}px);
         }
         100%{
             transform: translate(0, 0);
@@ -18,14 +18,14 @@ function Orb() {
     `
 
   const OrbStyled = styled.div`
-    width: 120vh;
-    height: 120vh;
+    width: 150vh;
+    height: 150vh;
     position: absolute;
     border-radius: 50%;
     margin-left: -37vh;
     margin-top: -37vh;
     background: linear-gradient(180deg, #f56692 0%, #f2994a 100%);
-    filter: blur(300px);
+    filter: blur(350px);
     animation: ${moveOrb} 15s alternate linear infinite;
   `
 
