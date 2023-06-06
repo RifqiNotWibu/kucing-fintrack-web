dad63db3645a2e4050f0baceb3023c7d54433ca2
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-06 10:23:55 +0700

    Overhauled Dashboard layout

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -23,113 +23,113 @@ function Dashboard() {
   }, [])
 
   return (
-    <DashboardStyled>
-      <InnerLayout>
-        <h1>All Transactions</h1>
-        <div className='stats-con'>
-          <div className='chart-con'>
-            <Chart />
-            <div className='amount-con'>
-              <div className='income'>
-                <h2>Total Income</h2>
-                <p>Rp {totalIncome()}</p>
+    <main>
+      <DashboardStyled>
+        <InnerLayout>
+          <div className='stats-con'>
+            <div className='chart-con'>
+              <Chart />
+              <div className='amount-con'>
+                <div className='income'>
+                  <h2>Total Income</h2>
+                  <p>Rp {totalIncome()}</p>
+                </div>
+                <div className='expense'>
+                  <h2>Total Expense</h2>
+                  <p>Rp {totalExpense()}</p>
+                </div>
               </div>
-              <div className='expense'>
-                <h2>Total Expense</h2>
-                <p>Rp {totalExpense()}</p>
+            </div>
+            <div className='history-con'>
+              <History />
+              <h2 className='income-title'>
+                Min<span>Income</span>Max
+              </h2>
+              <div className='income-item'>
+                <p>Rp {Math.min(...incomes.map((item) => item.amount))}</p>
+                <p>Rp {Math.max(...incomes.map((item) => item.amount))}</p>
               </div>
-              <div className='balance'>
-                <h2>Total Balance</h2>
-                <p>Rp {totalBalance()}</p>
+              <h2 className='expense-title'>
+                Min<span>Expense</span>Max
+              </h2>
+              <div className='expense-item'>
+                <p>Rp {Math.min(...expenses.map((item) => item.amount))}</p>
+                <p>Rp {Math.max(...expenses.map((item) => item.amount))}</p>
+              </div>
+            </div>
+            <div className='balance'>
+              <div className='balance-box'>
+                <h2>
+                  Total Balance : <span> Rp {totalBalance()}</span>{' '}
+                </h2>
               </div>
             </div>
           </div>
-          <div className='history-con'>
-            <History />
-            <h2 className='income-title'>
-              Min <span>Income</span>Max
-            </h2>
-            <div className='income-item'>
-              <p>Rp {Math.min(...incomes.map((item) => item.amount))}</p>
-              <p>Rp {Math.max(...incomes.map((item) => item.amount))}</p>
-            </div>
-            <h2 className='expense-title'>
-              Min <span>Expense</span>Max
-            </h2>
-            <div className='expense-item'>
-              <p>Rp {Math.min(...expenses.map((item) => item.amount))}</p>
-              <p>Rp {Math.max(...expenses.map((item) => item.amount))}</p>
-            </div>
-          </div>
-        </div>
-      </InnerLayout>
-    </DashboardStyled>
+        </InnerLayout>
+      </DashboardStyled>
+    </main>
   )
 }
 
 const DashboardStyled = styled.div`
+  padding-right: 1rem;
+  padding-left: 1rem;
     .stats-con{
         display: grid;
         -ms-layout-grid-line: none;
         grid-template-columns: repeat(5, 1fr);
         gap: 2rem;
         .chart-con{
-            grid-column: 1 / 4;
-            height: 400px;
+            grid-column: 1/4;
+            height: 425px;
             .amount-con{
                 display: grid;
                 grid-template-columns: repeat(4, 1fr);
                 gap: 2rem;
-                margin-top: 3.5rem;
+                margin-top: 1.5rem;
                 .income, .expense{
                     grid-column: span 2;
+                    font-size: 1rem;
                 }
-                .income,.balance{
+                .income{
+                  background: rgba(252, 246, 249, 0.6);
+                  backdrop-filter: blur(4.5px);
+                  border: 2px solid #FFFFFF;
+                  box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+                  border-radius: 20px;
+                  padding-top: 0.8rem;
+                  padding-left: 1rem;
+                  height: 100px;
                   color: green;
-                    background: #FCF6F9;
-                    border: 2px solid #FFFFFF;
-                    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
-                    border-radius: 20px;
-                    padding: 1rem;
-                    h2{
+                  h2{
+                    color: green;
+                    opacity: 0.8;
+                  }
+                  p{
+                      font-size: 1.75rem;
+                      font-weight: 700;
                       color: green;
-                      opacity: 0.8;
-                    }
-                    p{
-                        font-size: 2.5rem;
-                        font-weight: 700;
-                        color: green;
-                        opacity: 0.6;
-                    }
+                      opacity: 0.6;
+                  }
                 }
                 .expense{
-                    background: #FCF6F9;
+                    background: rgba(252, 246, 249, 0.6);
+                    backdrop-filter: blur(4.5px);
                     border: 2px solid #FFFFFF;
                     box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
                     border-radius: 20px;
-                    padding: 1rem;
+                    padding-top: 0.8rem;
+                    padding-left: 1rem;
+                    height: 100px;
                     color: red;
                     h2{
                       color: red;
                     }
                     p{
-                        font-size: 2.5rem;
+                        font-size: 1.75rem;
                         font-weight: 700;
                         color: red;
                         opacity: 0.6;
-                    }
-                }
-
-                .balance{
-                    grid-column: 1 / 5;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                    p{
-                        color: var(--color-green);
-                        opacity: 0.6;
-                        font-size: 3.5rem;
                     }
                 }
             }
@@ -138,28 +138,32 @@ const DashboardStyled = styled.div`
         .history-con{
             grid-column: 4 / -1;
             h2{
-                margin: 1rem 0;
+                margin: 0.9rem 0;
                 display: flex;
                 align-items: center;
                 justify-content: space-between;
             }
             .income-title{
                 font-size: 1.2rem;
+                color: green;
+                opacity: 0.8;
+                padding-left: 1rem;
+                padding-right: 1rem;
                 span{
                     font-size: 1.8rem;
                 }
-                color: green;
-                opacity: 0.8;
             }
             .income-item{
-                background: #FCF6F9;
-                border: 2px solid #FFFFFF;
-                box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
-                padding: 1rem;
-                border-radius: 20px;
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
+              display: flex;
+              background: rgba(252, 246, 249, 0.6);
+              backdrop-filter: blur(4.5px);
+              border: 2px solid #FFFFFF;
+              box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+              margin-top: -1rem;
+              padding: 1rem;
+              border-radius: 20px;
+              justify-content: space-between;
+              align-items: center;
                 p{
                     font-weight: 600;
                     font-size: 1.6rem;
@@ -167,16 +171,21 @@ const DashboardStyled = styled.div`
             }
             .expense-title{
                 font-size: 1.2rem;
+                color: red;
+                opacity: 0.8;
+                padding-left: 1rem;
+                padding-right: 1rem;
                 span{
                     font-size: 1.8rem;
                 }
-                color: red;
             }
             .expense-item{
-                background: #FCF6F9;
+                background: rgba(252, 246, 249, 0.6);
+                backdrop-filter: blur(4.5px);
                 border: 2px solid #FFFFFF;
-                box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
                 padding: 1rem;
+                margin-top: -1rem;
+                box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
                 border-radius: 20px;
                 display: flex;
                 justify-content: space-between;
@@ -188,6 +197,33 @@ const DashboardStyled = styled.div`
             }
         }
     }
+    .balance {
+      display: flex;
+      grid-column: 1 / 6;
+      justify-content: center;
+      background: rgba(252, 246, 249, 0.4);
+      backdrop-filter: blur(4.5px);
+      box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+      border-radius: 20px;
+      padding: 1rem;
+      .balance-box{
+        display: flex;
+        flex-direction: column;
+        h2{
+          color: green;
+          opacity: 0.8;
+        }
+        span{
+          margin-left: 1rem;
+        }
+      p{
+          font-size: 1.75rem;
+          font-weight: 700;
+          color: green;
+          opacity: 0.6;
+      }
+      }
+   }
 `
 
 export default Dashboard
