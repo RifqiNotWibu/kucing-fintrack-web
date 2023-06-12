b9a29563992c3877e87cad845fdc5666652f3f98
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-12 22:17:04 +0700

    Added redux store code

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import authReducer from './reducers'
+
+const store = createStore(
+  authReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
+export default store
