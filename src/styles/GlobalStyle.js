89148ee4ade85b5136e0b971c606f505cb864f6d
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-14 12:09:17 +0700

    Added global color

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -12,7 +12,8 @@ export const GlobalStyle = createGlobalStyle`
     --primary-color: #222260;
     --primary-color2: 'color: rgba(34, 34, 96, .6)';
     --primary-color3: 'color: rgba(34, 34, 96, .4)';
-    --color-green: ##4fc22b;
+    --color-green: #4fc22b;
+    --color-red: #ff0000;
     --color-grey: #aaa;
     --color-accent: #F56692;
     --color-delete: #FF0000;
